d5414aefad8532898dd47fb4e57582aa2a2573e2
Julienne H <juliennehattabi@gmail.com>
2024-11-07 11:08:18 +0100

    feat : modification of images and card layout for better UX

--- portfolio-hattabi-julienne/src/components/CardWorks.jsx
+++ portfolio-hattabi-julienne/src/components/CardWorks.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const CardWorks = ({ title, screen, onClick }) => {
   return (
     <div className="card" onClick={onClick}>
-      <h4 className="card_title">{title}</h4>
+      <div className="card_text">
+        <h4 className="card_title">{title}</h4>
+      </div>
       <img src={screen} alt={title} className="card_image" />
     </div>
   );
